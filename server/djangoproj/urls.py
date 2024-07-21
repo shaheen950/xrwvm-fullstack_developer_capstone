ef444eafcb185d3956a703b900a4de4596259e3a
shaheen950 <ibraissus@gmail.com>
2024-07-21 20:11:46 +0300

    z

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,9 +18,13 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('djangoapp/logout/', views.logout_request, name='logout'),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
